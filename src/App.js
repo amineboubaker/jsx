7bc87e469fca65400828fb7a976b80619220ccfc
amineboubaker <amine.boubaker001@gmail.com>
2021-04-28 15:01:11 +0200

    jsx project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./components/imageInSrc.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+<div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+
+
+<h1 className="title red">Your name here</h1>
+
+
+<img className="pub2" src={logo}/>  
+
+
+          
+<img className="pub" src="/imageInPublic.jpg" /> </div>
+
+ 
+
+     
     </div>
+    
+    
+
   );
 }
 
